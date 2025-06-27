dcb26aff720459be063c5d34da337649a9927b6d
2313tiny <mherogalstyan@gmail.com>
2025-06-27 11:31:13 +0400

    review buffer functionality

--- task-1.cpp
+++ task-1.cpp
@@ -37,6 +37,8 @@
 #include <stdlib.h>
 #include <common/mavlink.h>
 //#include </home/agmer/mavlink/generated/include/mavlink/v2.0/common/mavlink.h>
+#include <mavlink_get_info.h>
+#include <protocol.h>
 
 void sf_readMessageFromFile(const char * filename){
 	FILE * file = fopen(filename, "r");
@@ -45,15 +47,65 @@ void sf_readMessageFromFile(const char * filename){
 		return;	
 	}
 
-
-	char buffer[256]; //Buffer to hold each line/ message
-	while(NULL != fgets(buffer, sizeof(buffer), file)){
+//	char buffer[253]; //Buffer to hold each line/ message
+/*	while(NULL != fgets(buffer, sizeof(buffer), file)){
 		//Process the message in buffer
-		printf("Read message: %s", buffer);	
+		printf("Read message: %s \n", buffer);	
 	}
+*/
+
+	//STX magic
+	//char buffer[253]; //Buffer to hold each line/ message
+	//uint8_t buffer[MAVLINK_STX]; //Buffer to hold each line/ message
+	char buffer[MAVLINK_STX]; //Buffer to hold each line/ message
+	fgets(buffer, sizeof(buffer), file);
+	printf("magic: %s \n", buffer);	
+
+	//LEN	
+	fgets(buffer, sizeof(buffer), file);
+	printf("len: %s \n", buffer);	
+
+
+	//INCOMPAT_FLAG	
+	fgets(buffer, sizeof(buffer), file);
+	printf("inc_flags: %s\n", buffer);
+
+	//COMPAT_FLAGs
+	fgets(buffer, sizeof(buffer), file);
+	printf("compat_flags: %s\n", buffer);
+
+
+	//PACKET SEQUENCE NUMBER
+	fgets(buffer, sizeof(buffer), file);
+	printf("packet sequence number: %s\n", buffer);
+	
+	//SYSTEM ID (sender)
+	fgets(buffer, sizeof(buffer), file);
+	printf("System ID: %s\n", buffer);
+	
+	//COMPONENT ID (sender)
+	fgets(buffer, sizeof(buffer), file);
+	printf("Component ID: %s\n", buffer);
+	
+
+	//Message ID	
+	fgets(buffer, sizeof(buffer), file);
+	printf("Message ID1: %s\n", buffer);
+	fgets(buffer, sizeof(buffer), file);
+	printf("Message ID2: %s\n", buffer);
+	fgets(buffer, sizeof(buffer), file);
+	printf("Message ID3: %s\n", buffer);
+	fgets(buffer, sizeof(buffer), file);
+	printf("Message ID4: %s\n", buffer);
+
+	
+
+
+		
 
 
 
+	
 	fclose(file); //Close the file
 }
 
@@ -61,7 +113,7 @@ void sf_readMessageFromFile(const char * filename){
 
 int main() {
 	printf("MAVLINK_STX = %d \n", MAVLINK_STX);	
-	printf("MAVLINK_WIRE_PROTOCOL_VERSION = %c \n ", MAVLINK_WIRE_PROTOCOL_VERSION);
+//	printf("MAVLINK_WIRE_PROTOCOL_VERSION = %c \n ", MAVLINK_WIRE_PROTOCOL_VERSION);
 
 	const char * filename = "mavlink.bin";
 	sf_readMessageFromFile(filename);
